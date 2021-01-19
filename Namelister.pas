c60acd59914ff088f1273a9918f617a87ed75427
geroborch <gero.borchhardt@yahoo.com>
2021-01-19 17:23:52 -0400

    <this is the real first pascal pro>

---
+++ Namelister.pas
@@ -0,0 +1,38 @@
+program NameLister;
+const TOTAL_NAMES = 5;
+type NameData = array [0..TOTAL_NAMES] of string; 
+
+function GetNames(): NameData;
+var i: integer;
+begin
+    WriteLn('Please enter you names:');
+    for i := 0 to TOTAL_NAMES do
+        ReadLn(result[i]);
+end;
+
+procedure WriteNames(names: NameData);
+var i: integer;
+begin   
+    WriteLn('');
+    WriteLn('Your names are:');
+    for i := 0 to TOTAL_NAMES do
+        case (names[i]) of
+            'John Egbert': WriteLn('Zoosmell Pooplord');
+            'Dave Strider': WriteLn('Insufferable Prick');
+            'Rose Lalonde': WriteLn('Flighty Broad');
+        else
+            WriteLn(names[i]);
+        end;
+end;
+
+procedure Main();
+var names: NameData;
+begin
+    names := GetNames();
+    WriteNames(names);
+end;
+
+begin 
+    Main();
+
+    end.
